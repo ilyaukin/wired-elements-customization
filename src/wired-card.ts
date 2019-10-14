b53b9cb3d0e29134701667fca0c156e8407dec70
Preet Shihn <preetshihn@gmail.com>
2019-10-14 15:39:27 -0700

    updated code to use new base class

--- src/wired-card.ts
+++ src/wired-card.ts
@@ -1,104 +1,87 @@
-import { WiredBase, customElement, property, TemplateResult, html, css, CSSResult } from 'wired-lib/lib/wired-base';
-import { rectangle, line } from 'wired-lib';
+import { WiredBase, BaseCSS, ResizeObserver } from 'wired-lib/lib/wired-base';
+import { rectangle, line, Point } from 'wired-lib';
+import { customElement, property, css, TemplateResult, html, CSSResultArray } from 'lit-element';
 
 @customElement('wired-card')
 export class WiredCard extends WiredBase {
   @property({ type: Number }) elevation = 1;
+  private resizeObserver?: ResizeObserver;
+  private windowResizeHandler?: EventListenerOrEventListenerObject;
 
-  private resizeHandler?: EventListenerOrEventListenerObject;
+  constructor() {
+    super();
+    if ((window as any).ResizeObserver) {
+      this.resizeObserver = new (window as any).ResizeObserver(() => {
+        if (this.svg) {
+          this.wiredRender();
+        }
+      });
+    }
+  }
 
-  static get styles(): CSSResult {
-    return css`
-    :host {
-      display: inline-block;
-      position: relative;
-      padding: 10px;
-      opacity: 0;
-    }
-
-    :host(.wired-rendered) {
-      opacity: 1;
-    }
-  
-    .overlay {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      pointer-events: none;
-    }
-  
-    svg {
-      display: block;
-    }
-  
-    path {
-      stroke: currentColor;
-      stroke-width: 0.7;
-      fill: transparent;
-    }
-    `;
+  static get styles(): CSSResultArray {
+    return [
+      BaseCSS,
+      css`
+        :host {
+          display: inline-block;
+          position: relative;
+          padding: 10px;
+        }
+      `
+    ];
   }
 
   render(): TemplateResult {
     return html`
     <div>
-      <slot @slotchange="${() => this.requestUpdate()}"></slot>
+      <slot @slotchange="${this.wiredRender}"></slot>
     </div>
-    <div class="overlay">
-      <svg id="svg"></svg>
-    </div>
+    <div id="overlay"><svg></svg></div>
     `;
   }
 
-  connectedCallback() {
-    super.connectedCallback();
-    if (!this.resizeHandler) {
-      this.resizeHandler = this.debounce(this.updated.bind(this), 200, false, this);
-      window.addEventListener('resize', this.resizeHandler);
-    }
-    setTimeout(() => this.updated());
+  updated() {
+    super.updated();
+    this.attachResizeListener();
   }
 
   disconnectedCallback() {
-    if (super.disconnectedCallback) super.disconnectedCallback();
-    if (this.resizeHandler) {
-      window.removeEventListener('resize', this.resizeHandler);
-      delete this.resizeHandler;
+    this.detachResizeListener();
+  }
+
+  private attachResizeListener() {
+    if (this.resizeObserver && this.resizeObserver.observe) {
+      this.resizeObserver.observe(this);
+    } else if (!this.windowResizeHandler) {
+      this.windowResizeHandler = () => this.wiredRender();
+      window.addEventListener('resize', this.windowResizeHandler, { passive: true });
     }
   }
 
-  private debounce(func: Function, wait: number, immediate: boolean, context: HTMLElement): EventListenerOrEventListenerObject {
-    let timeout = 0;
-    return () => {
-      const args = arguments;
-      const later = () => {
-        timeout = 0;
-        if (!immediate) {
-          func.apply(context, args);
-        }
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = window.setTimeout(later, wait);
-      if (callNow) {
-        func.apply(context, args);
-      }
-    };
+  private detachResizeListener() {
+    if (this.resizeObserver && this.resizeObserver.unobserve) {
+      this.resizeObserver.unobserve(this);
+    }
+    if (this.windowResizeHandler) {
+      window.removeEventListener('resize', this.windowResizeHandler);
+    }
   }
 
-  updated() {
-    const svg = (this.shadowRoot!.getElementById('svg') as any) as SVGSVGElement;
-    while (svg.hasChildNodes()) {
-      svg.removeChild(svg.lastChild!);
-    }
+  protected canvasSize(): Point {
     const s = this.getBoundingClientRect();
     const elev = Math.min(Math.max(1, this.elevation), 5);
     const w = s.width + ((elev - 1) * 2);
     const h = s.height + ((elev - 1) * 2);
-    svg.setAttribute('width', `${w}`);
-    svg.setAttribute('height', `${h}`);
+    return [w, h];
+  }
+
+  protected draw(svg: SVGSVGElement, size: Point) {
+    const elev = Math.min(Math.max(1, this.elevation), 5);
+    const s = {
+      width: size[0] - ((elev - 1) * 2),
+      height: size[1] - ((elev - 1) * 2)
+    };
     rectangle(svg, 2, 2, s.width - 4, s.height - 4);
     for (let i = 1; i < elev; i++) {
       (line(svg, (i * 2), s.height - 4 + (i * 2), s.width - 4 + (i * 2), s.height - 4 + (i * 2))).style.opacity = `${(85 - (i * 10)) / 100}`;
@@ -106,6 +89,5 @@ export class WiredCard extends WiredBase {
       (line(svg, (i * 2), s.height - 4 + (i * 2), s.width - 4 + (i * 2), s.height - 4 + (i * 2))).style.opacity = `${(85 - (i * 10)) / 100}`;
       (line(svg, s.width - 4 + (i * 2), s.height - 4 + (i * 2), s.width - 4 + (i * 2), i * 2)).style.opacity = `${(85 - (i * 10)) / 100}`;
     }
-    this.classList.add('wired-rendered');
   }
 }
